ec85521ef8f8a923a18172b5e508fd53c5117826
Gr3mLin404 <maksim.boyarkin96@gmail.com>
2020-05-15 18:02:49 +0300

    Builder fix

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ import os
 import colorama
 
 
-class Installer:
+class Builder:
     info = colorama.Fore.BLUE
     ok = colorama.Fore.GREEN
     warning = colorama.Fore.LIGHTRED_EX
@@ -16,42 +16,45 @@ class Installer:
     vs32 = '"Visual Studio 15 2017"'
     vs64 = '"Visual Studio 16 2019"'
     ninja = 'Ninja'
-
+    usage = 'sudo python3 install.py -b ninja|vs32|vs64 -p 8'
+    descript = '''The number of threads in the -p flag is indicated from\
+    the calculation: the number of processor cores * 2 + 1'''
+    
     def check_depends(self):
-        print(Installer.info, 'Install Dependent Libraries', Installer.reset)
+        print(Builder.info, 'Install Dependent Libraries', Builder.reset)
         time.sleep(3)
-        if 'MAJARO' or 'ARCH' in Installer.release:
+        if 'MAJARO' or 'ARCH' in Builder.release:
             os.system('pacman -S --needed cmake gcc ninja openssl')
-        elif 'Ubuntu' in Installer.release:
+        elif 'Ubuntu' in Builder.release:
             os.system('apt-get install build-essential autoconf libtool pkg-config libgflags-dev\
             libgtest-dev clang libc++-dev -y')
-        elif 'Windows' in Installer.system:
+        elif 'Windows' in Builder.system:
             os.environ['CC']='cl.exe'
             os.environ['CXX']='cl.exe'
         else:
-            print(Installer.critical, 'ERROR! Platform not supported!', Installer.reset)
+            print(Builder.critical, 'ERROR! Platform not supported!', Builder.reset)
 
     def building(self, args):
         if os.path.isdir(r"build") != True:
             os.mkdir(r"build")
         os.chdir('./build')
         if args.build[0] == 'ninja':
-            os.system(f'cmake .. -G {Installer.ninja}')
-            os.system(f'cmake --build . --config Release --parallel {args.parallel[0]}')
-            if args[0] == 'vs32':
-                os.system(f'cmake .. -G {Installer.vs32}')
-            if args[0] == 'vs64':
-                os.system(f'cmake .. -G {Installer.vs64}')
+            os.system(f'cmake .. -G {Builder.ninja}')
+        elif args.build[0] == 'vs32':
+            os.system(f'cmake .. -G {Builder.vs32}')
+        elif args.build[0] == 'vs64':
+            os.system(f'cmake .. -G {Builder.vs64}')
+        os.system(f'cmake --build . --config Release --parallel {args.parallel[0]}')
 
     def parse_args(self):
-        parser = argparse.ArgumentParser(usage='sudo python3 install.py -b ninja|vs32|vs64 -p 16')
+        parser = argparse.ArgumentParser(usage=Builder.usage, description=Builder.descript)
         parser.add_argument('-b', '--build', nargs=1, required=True, choices=['ninja', 'vs32', 'vs64'])
-        parser.add_argument('-p', '--parallel', nargs=1, required=True, choices=['16'])
+        parser.add_argument('-p', '--parallel', nargs=1, default='2')
         return parser.parse_args()
 
 
 if __name__ == '__main__':
-    install = Installer()
+    install = Builder()
     args = install.parse_args()
     install.check_depends()
     install.building(args)
